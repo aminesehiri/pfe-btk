7065753a5d3acd7a1907437ca7b8e6431a50d2ed
shiri <shiriamine2@gmail.com>
2024-06-05 23:43:11 +0100

    ll

--- src/components/BankInfoSection/BankInfoSection.jsx
+++ src/components/BankInfoSection/BankInfoSection.jsx
@@ -1,23 +1,86 @@
 import React from 'react';
 import './bankinfosectionCss.css';
-
+import imgamine from '../../assest/amine.jpg';
+import imgouday from '../../assest/ouday.png';
+import imgabir from '../../assest/abir.png';
+import pythonIcon from '../../assest/python.jpg';
+import talendIcon from '../../assest/talend.png';
+import postgresIcon from '../../assest/postgres.png';
+import qliksenseIcon from '../../assest/qlik.png';
+import Footer from '../Footer/Footer';
 
 function BankInfoSection() {
-
   return (
-    <section className="bank-info-section">
-      <h2>À propos de la BTK</h2>
-      <p>
-        La BTK (Banque Tuniso-Koweïtienne) est une institution financière de premier plan en Tunisie. Fondée en 1980, elle offre une gamme complète de services bancaires aux particuliers, entreprises et institutions. La BTK s'engage à fournir des solutions financières innovantes et adaptées aux besoins de ses clients.
-      </p>
-      <iframe 
-        src="http://localhost:4848/single/?appid=C%3A%5CUsers%5Cshiri%5COneDrive%5CDocuments%5CQlik%5CSense%5CApps%5Ctttt.qvf&obj=jhgZpvJ&theme=horizon&opt=nointeraction,noselections"  
-        style={{ border: 'none', width: '100%', height: '100%' }}
-      ></iframe>
-      <p>
-        La BTK dispose de 32 agences réparties à travers le pays, assurant une couverture nationale pour mieux servir ses clients. Grâce à une équipe de professionnels dévoués, la BTK continue de renforcer sa position sur le marché bancaire tunisien.
-      </p>
-    </section>
+    <div className="container">
+      <section className="bank-info-section">
+        <center><h2>À propos de la BTK</h2></center>
+        <p>
+          La BTK (Banque Tuniso-Koweïtienne) est une institution financière de premier plan en Tunisie. Fondée en 1980, elle offre une gamme complète de services bancaires aux particuliers, entreprises et institutions. La BTK s'engage à fournir des solutions financières innovantes et adaptées aux besoins de ses clients.
+        </p>
+        
+        <p>
+          La BTK dispose de 32 agences réparties à travers le pays, assurant une couverture nationale pour mieux servir ses clients. Grâce à une équipe de professionnels dévoués, la BTK continue de renforcer sa position sur le marché bancaire tunisien.
+        </p>
+
+        <section className="team-section">
+          <center><h2>Notre Équipe</h2></center> <br /><br />
+          <div className="cards">
+            <a href="https://www.linkedin.com/in/abirjemaii/" target="_blank" rel="noopener noreferrer" className="card">
+              <img src={imgabir} alt="Abir Jemai" />
+              <h3>Abir Jemai</h3>
+              <p>Responsable BI et CRM</p>
+            </a>
+            <a href="https://www.linkedin.com/in/medaminesehiri/" target="_blank" rel="noopener noreferrer" className="card">
+              <img src={imgamine} alt="Mohamed Amine Sehiri" />
+              <h3>Mohamed Amine Sehiri</h3>
+              <p>Ingénieur Web et BI</p>
+            </a>
+            <a href="https://www.linkedin.com/in/oudaydjobbi/" target="_blank" rel="noopener noreferrer" className="card">
+              <img src={imgouday} alt="Ouday Djobbi" />
+              <h3>Ouday Djobbi</h3>
+              <p>Ingénieur Web et BI</p>
+            </a>
+          </div>
+        </section>
+        <br />
+        <section className="tools-section">
+          <center><h2>Nos outils de création des tableaux de bord</h2></center>
+          <br /><br />
+          <div className="tools">
+            <div className="tool">
+              <img src={pythonIcon} alt="Python" />
+              <p>Python</p>
+            </div>
+            <div className="tool">
+              <img src={talendIcon} alt="Talend" />
+              <p>Talend</p>
+            </div>
+            <div className="tool">
+              <img src={postgresIcon} alt="PostgreSQL" />
+              <p>PostgreSQL</p>
+            </div>
+            <div className="tool">
+              <img src={qliksenseIcon} alt="Qlik Sense" />
+              <p>Qlik Sense</p>
+            </div>
+          </div>
+        </section>
+        <br />
+        <section className="process-section">
+          <center><h2>Notre processus pour la création des tableaux de bord</h2></center>
+          <br /><br />
+          <ol>
+            <li>Prendre les données sous forme des fichiers Excel à partir de notre responsable BI</li>
+            <li>Faire le traitement nécessaire avec Python</li>
+            <li>Créer et charger les données dans des datamarts PostgreSQL avec Talend</li>
+            <li>Créer les visualisations avec Qlik Sense</li>
+            <li>Intégrer les tableaux de bord dans notre site web</li>
+          </ol>
+        </section>
+        <br /><br /><br />
+      </section>
+      <Footer />
+    </div>
   );
 }
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import './Footer.css'; // Optional: for any footer-specific styles
 const Footer = () => {
   return (
     <footer>
-      <p>Développé par Mohamed Amine Sehiri et Ouday Djobbi, étudiants de l'ESSECT</p>
+      <p>Développé par Mohamed Amine Sehiri et Ouday Djobbi.</p>
     </footer>
   );
 };
